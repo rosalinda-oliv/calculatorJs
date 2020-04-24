32daa16d2bb2ab4691c54b9769092166b0fd91ee
Rosalinda Oliveira <rosimartinsoliveira@gmail.com>
2020-04-24 20:40:20 +0100

    operation

--- script.js
+++ script.js
@@ -17,20 +17,61 @@ class Calculator {
     }
 
     appendNumber(number) {
-        this.currentOperand = number;
+        if (number === '.' && this.currentOperand.includes('.')) return
+
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+
+        if (this.previousOperand !== '') {
+            this.calculator();
+        }
+
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
 
     }
 
-    chooseOperation(operation){
+    compute() {
+        let computation;
+
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+
+        if (isNaN(prev) || isNaN(current)) return
+
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+
+            case '-':
+                computation = prev - current;
+                break;
+
+            case '*':
+                computation = prev * current;
+                break;
+
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+
 
     }
 
-    compute(){
-
-    }
-
-    updateDisplay(){
+    updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previousOperandTextElement.innerText = this.previousOperand;
 
     }
 }
@@ -43,11 +84,23 @@ const allClearButtons = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
-const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement);
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
 numberButtons.forEach(button => {
-    button.addEventListener('click', () =>{
+    button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText);
         calculator.updateDisplay();
-    });
+    })
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+});
+
+equalsButtons.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
 });
